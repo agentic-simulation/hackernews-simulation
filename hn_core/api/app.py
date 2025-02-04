90782ae9618541203a3820b4d31e8ac92580ba45
Daniel Lee <dannylee1020@gmail.com>
2025-02-03 20:38:00 -0800

    feat: endpoint for starting simulation

---
+++ hn_core/api/app.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from hn_core.simulation import run as simulation
+
+from .model import PostRequest, SimulationRequest
+
+app = FastAPI()
+
+
+@app.post("/run")
+def run(request: PostRequest):
+    """endpoint for running simulatio"""
+    try:
+        result = simulation.run(
+            title=request.title,
+            url=request.url,
+            text=request.text,
+            model="gpt-4o-mini",
+            num_agents=request.param.num_agents,
+            total_time_steps=request.param.total_time_steps,
+            batch_size=request.param.batch_size,
+            k=request.param.k,
+        )
+
+        return JSONResponse(content=result)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
